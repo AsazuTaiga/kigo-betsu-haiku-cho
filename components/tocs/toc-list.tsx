7e304f47614e108696275aeb67f8886397ea7897
Asazu Taiga <taiga.asazu@icloud.com>
2020-12-15 00:19:27 +0900

    WIP: read local markdown in getStaticPorps

---
+++ components/tocs/toc-list.tsx
@@ -0,0 +1,31 @@
+type Props = {
+  tocList: Toc[]
+}
+
+type Toc = {
+  title: string
+  id: string
+}
+
+const TocList: React.FC<Props> = (props) => {
+  const { tocList } = { ...props }
+  return (
+    <>
+      <ul className="tocList">
+        {tocList.map((toc, index) => (
+          <li key={index}>
+            <a href={`#${toc.id}`}>{toc.title}</a>
+          </li>
+        ))}
+      </ul>
+      <style jsx>
+        {`
+          .tocList li {
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TocList
---
+++ pages/site.tsx
@@ -0,0 +1,34 @@
+import fs from 'fs'
+import { NextPage, GetStaticProps } from 'next'
+// import TocList from '../components/tocs/toc-list'
+import Markdwon from 'react-markdown'
+
+type InitialProps = {
+  privacy: string
+  terms: string
+}
+
+const Site: NextPage<InitialProps> = (props) => {
+  const { privacy, terms } = { ...props }
+  return (
+    <>
+      <main>
+        <Markdwon skipHtml={true}>{terms}</Markdwon>
+        <Markdwon skipHtml={true}>{privacy}</Markdwon>
+      </main>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const privacy = fs.readFileSync(process.cwd() + '/docs/privacy.md', 'utf8')
+  const terms = fs.readFileSync(process.cwd() + '/docs/terms.md', 'utf8')
+  return {
+    props: {
+      privacy: privacy,
+      terms: terms,
+    },
+  }
+}
+
+export default Site
